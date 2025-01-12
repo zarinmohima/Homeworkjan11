0863c74b186c4e00c4ad20f90fbef983da1d9fac
zarinmohima <zarintasnimmohima@gmail.com>
2025-01-11 21:36:00 -0500

    I am solving the problems now

--- homework.py
+++ homework.py
@@ -1,8 +1,24 @@
-Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
-Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
-Declare a variable named company and assign it to an initial value "Coding For All".
-Print the variable company using print().
-Print the length of the company string using len() method and print().
-Change all the characters to uppercase letters using upper() method.
-Change all the characters to lowercase letters using lower() method.
+#Q1.Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+words = ['Thirty', 'Days', 'Of', 'Python']
+# I need to Join the words with space separator, xplicit spaces (' ') 
+result = ' '.join(words)
+print(result)
+
+#Q2.Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+# words needs to be Listed to concatenate
+words = ['Coding', 'For', 'All']
+# Join + variable
+result = ' '.join(words)
+print(result)
+
+#Q3.Declare a variable named company and assign it to an initial value "Coding For All".
+# Declare a variable and assign the value Coding For All
+company = "Coding For All"
+# Print the variable 
+print(company)
+
+#Print the variable company using print().
+#Print the length of the company string using len() method and print().
+#Change all the characters to uppercase letters using upper() method.
+#Change all the characters to lowercase letters using lower() method.
 
